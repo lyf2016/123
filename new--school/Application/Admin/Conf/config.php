51d1e21fca6f287f161dd76b79b3a8d864cc18cc
lyf2016 <https://github.com/lyf2016/123.git>
2017-03-30 11:45:45 +0800

    Signed-off-by: lyf2016 <https://github.com/lyf2016/123.git>

--- new--school/Application/Admin/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    // '配置项' => '配置值'
-    'LOAD_EXT_CONFIG'       => 'db',
-    'ACCOUNT'               => array('NAME'=>'admin', 'PASSWD'=>'admin888'),
-    'EXPIRE'                => 60*30,
-    'PAGE_COUNT'            => 18,
-);
--- new--school/Application/Common/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    //'配置项'=>'配置值'
-    'URL_MODEL'=>0,     //URL模式选择
-    'UPLOAD_FILE_MAX_BYTES'=>200*1024*1024, //上传文件大小，单位字节
-    'UPLOAD_FILE_TYPE'=>array('jpg', 'gif', 'jpeg', 'png'), //上传图片后缀BIND_CONTROLLER
-    'DEFAULT_CONTROLLER'=>'News',
-    'DEFAULT_MODULE'        => 'Home',
-);
